ba04c93ebe1e7b3e516018a7179b429e4c084605
Oliver Runge <oliver.runge@gmail.com>
2015-08-30 19:55:34 +0200

    build.py

--- build.py
+++ build.py
@@ -6,12 +6,12 @@ from datetime import datetime
 print("starting with args: {}".format(sys.argv[1:]))
 
 print("{} did some things...".format(datetime.now()))
-time.sleep(0.1)
+time.sleep(2)
 print("{} did some more things...".format(datetime.now()))
-time.sleep(0.1)
+time.sleep(3)
 
 for i in range(150):
     print("line {}".format(i))
-    #time.sleep(0.1)
+    time.sleep(0.1)
 
 print("all done!")
