8c82f2d2a99306d38ab3d27615425dce4aed2836
Oliver Runge <oliver.runge@gmail.com>
2015-09-02 23:19:27 +0200

    Smarter react version

--- jeeves/core/static/js/progress_bar.jsx
+++ jeeves/core/static/js/progress_bar.jsx
@@ -17,6 +17,8 @@ function get_time_difference_string(num_seconds) {
     }
     if (result != '') {
       result = part + ', ' + result;
+    } else {
+      result = part;
     }
   }
 
@@ -36,76 +38,72 @@ function update_progress_bars() {
       pbar.data('initial-timestamp', $.now() / 1000);
     }
 
-    var initial_timestamp = pbar.data('initial-timestamp');
-    var initial_elapsed_time = pbar.data('initial-elapsed-time');
-    var estimated_time = pbar.data('estimated-time');
+    React.render(
+      React.createElement(ProgressBar, {
+        id: pbar.prop('id'),
+        initial_timestamp: pbar.data('initial-timestamp'),
+        initial_elapsed_time: pbar.data('initial-elapsed-time'),
+        estimated_time: pbar.data('estimated-time'),
+      }),
+      pbar.get()[0]
+    );
+  });
+}
 
-    var start_time = initial_timestamp - initial_elapsed_time;
-    var end_time = start_time + estimated_time;
-    var now = $.now() / 1000;
 
-    var elapsed_time = now - start_time;
+ProgressBar = React.createClass({
+  getInitialState() {
+    return {
+      now: $.now() / 1000,
+    }
+  },
+
+  render() {
+    setTimeout((function() {
+      this.setState({now: $.now() / 1000});
+    }).bind(this), 1000);
+
+    var start_time = this.props.initial_timestamp - this.props.initial_elapsed_time;
+    var end_time = start_time + this.props.estimated_time;
+
+    var elapsed_time = this.state.now - start_time;
     var elapsed_time_string = get_time_difference_string(elapsed_time);
 
     var normal_percentage;
     var overtime_percentage;
     var overtime = false;
 
-    if (estimated_time == null) {
+    if (this.props.estimated_time == null) {
       normal_percentage = 100.0 * (1.0 - Math.exp(-elapsed_time / 300.0));
     } else {
-      if (elapsed_time > estimated_time) {
+      if (elapsed_time > this.props.estimated_time) {
         overtime = true;
-        var over = elapsed_time - estimated_time;
-        var estimation_steps = Math.max(estimated_time / 10, 10)
-        var projected_eta = (Math.floor(over / estimation_steps) + 1) * estimation_steps + estimated_time;
+        var over = elapsed_time - this.props.estimated_time;
+        var estimation_steps = Math.max(this.props.estimated_time / 10, 10)
+        var projected_eta = (Math.floor(over / estimation_steps) + 1) * estimation_steps + this.props.estimated_time;
 
-        normal_percentage = 100.0 * estimated_time / projected_eta;
+        normal_percentage = 100.0 * this.props.estimated_time / projected_eta;
         overtime_percentage = 100.0 * over / projected_eta;
       } else {
-        normal_percentage = 100.0 * elapsed_time / estimated_time;
+        normal_percentage = 100.0 * elapsed_time / this.props.estimated_time;
       }
     }
 
-    React.render(
-      React.createElement(ProgressBar, {
-        'id': pbar.prop('id'),
-        'data_estimated_time': estimated_time,
-        'data_initial_timestamp': initial_timestamp,
-        'data_initial_elapsed_time': initial_elapsed_time,
-        'duration': elapsed_time_string,
-        'overtime': overtime,
-        'normal_percentage': normal_percentage,
-        'overtime_percentage': overtime_percentage,
-      }),
-      pbar.get()[0]
-    );
-  });
-
-  setTimeout(update_progress_bars, 5000);
-}
-
-
-ProgressBar = React.createClass({
-  render() {
     var overtime_bar;
-    if (this.props.overtime) {
+    if (overtime) {
       overtime_bar = <div
-          className="progress-bar progress-bar-warning active progress-bar-striped"
-          style={{width: this.props.overtime_percentage + "%" }} />;
+          className="progress-bar progress-bar-warning progress-bar-striped active"
+          style={{width: overtime_percentage + "%" }} />;
     }
 
     return (
-      <div id={this.props.id }
-           className="progress"
-           data-initial-elapsed-time={this.props.data_initial_elapsed_time}
-           data-estimated-time={this.props.data_estimated_time}>
+      <div className="progress">
         <div className={
               "progress-bar progress-bar-success" +
-                (!this.props.overtime ? " active progress-bar-striped" : "")
+                (!overtime ? " progress-bar-striped active" : "")
              }
-             style={{width: this.props.normal_percentage + "%" }}
-             ><span>{this.props.duration}</span></div>
+             style={{width: normal_percentage + "%" }}
+             ><span>{elapsed_time_string}</span></div>
         {overtime_bar}
       </div>
     );
