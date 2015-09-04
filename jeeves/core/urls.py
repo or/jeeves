6190b911ea30cbdda38afdf2cc3061ca0b45bd85
Oliver Runge <oliver.runge@gmail.com>
2015-09-04 17:38:46 +0200

    Expect urls with trailing slashes
    This makes correctly /admin redirect to /admin/.

--- jeeves/core/urls.py
+++ jeeves/core/urls.py
@@ -8,19 +8,19 @@ urlpatterns = [
         login_required(views.ProjectListView.as_view()),
         name="project-list"),
 
-    url('^(?P<project_slug>[^/]+)/?$',
+    url('^(?P<project_slug>[^/]+)/$',
         login_required(views.BuildListView.as_view()),
         name="build-list"),
 
-    url('^(?P<project_slug>[^/]+)/(?P<build_id>\d+)/?$',
+    url('^(?P<project_slug>[^/]+)/(?P<build_id>\d+)/$',
         login_required(views.BuildDetailView.as_view()),
         name="build-view"),
 
-    url('^(?P<project_slug>[^/]+)/(?P<build_id>\d+)/reschedule/?$',
+    url('^(?P<project_slug>[^/]+)/(?P<build_id>\d+)/reschedule/$',
         login_required(views.BuildRescheduleView.as_view()),
         name="build-reschedule"),
 
-    url('^(?P<project_slug>[^/]+)/(?P<build_id>\d+)/log/?$',
+    url('^(?P<project_slug>[^/]+)/(?P<build_id>\d+)/log/$',
         login_required(views.BuildLogView.as_view()),
         name="build-log"),
 ]
--- jeeves/urls.py
+++ jeeves/urls.py
@@ -10,12 +10,12 @@ import jeeves.github.urls
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
-    url('^login/?$', login, name='login',
+    url('^login/$', login, name='login',
         kwargs={'template_name': 'login.html'}),
-    url('^logout/?$', logout, name='logout',
+    url('^logout/$', logout, name='logout',
         kwargs={'next_page': '/'}),
 
-    url(r'^github-webhook/?', include(jeeves.github.urls)),
+    url(r'^github-webhook/', include(jeeves.github.urls)),
     url(r'^', include(jeeves.core.urls)),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
