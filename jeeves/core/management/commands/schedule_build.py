ce333f75a5587c31ace4963b8abd8a642ae68859
Markus Bertheau <mbertheau@gmail.com>
2015-09-04 06:16:50 +0200

    Add management command to schedule a build
    For easier testing at home.

---
+++ jeeves/core/management/commands/schedule_build.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from jeeves.core.models import Project
+from jeeves.core.service import schedule_build
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('project')
+
+    def handle(self, *args, **options):
+        project = Project.objects.get(name=options['project'])
+        schedule_build(project, reason='Triggered from management command')
