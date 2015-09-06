7f0cbfc426af1a31075a52061203501f98583cd8
Oliver Runge <oliver.runge@gmail.com>
2015-09-06 09:47:38 +0200

    Remove old and wrong build count context processor

--- jeeves/context_processors.py
+++
@@ -1,8 +0,0 @@
-from jeeves.core.models import Build
-
-
-def navbar(request):
-    return {
-        'num_running_builds':
-        Build.objects.filter(status=Build.Status.RUNNING).count(),
-    }
