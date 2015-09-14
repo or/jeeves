f0c1b1ae99e57490d1a468b3cfa250160c12833a
Oliver Runge <oliver.runge@gmail.com>
2015-09-15 00:58:00 +0200

    Honour job dependencies
    The order of jobs now is based on their dependencies (and the order of
    their id where no dependency dictates an order).
    If a job depends on a job that finished with failure, then that job
    fails as well.
    
    If a job depends on a job that is unknown, then that, too, means the job
    fails. This should be prevented when saving the project already, by
    validating the job descriptions and their dependencies.

--- jeeves/core/service.py
+++ jeeves/core/service.py
@@ -94,65 +94,125 @@ def start_build(build_pk):
 
     build_start.send('core', build=build)
 
+    known_jobs = set()
     dependencies = {}
     for jd in build.project.jobdescription_set.all():
-        dependencies[jd.name] = \
-            jd.dependencies.split(', ') if jd.dependencies else []
+        known_jobs.add(jd.name.lower())
+        dependencies[jd.name.lower()] = \
+            [x.lower() for x in jd.dependencies.split(', ')] if jd.dependencies else []
 
+    result_map = {}
     all_passed = True
-    for job_description in build.project.jobdescription_set.order_by('id'):
-        job = Job(build=build, name=job_description.name)
-        job.status = Job.Status.RUNNING
-        job.log_file.save('build-{:05d}-{:05d}-{}.log'
-                          .format(build.project.id,
-                                  build.build_id,
-                                  job_description.name),
-                          File(StringIO()))
-        job.save()
+    jobs_to_do = list(build.project.jobdescription_set.order_by('id'))
+    while jobs_to_do:
+        jobs_left_to_do = []
+        for job_description in jobs_to_do:
+            has_unmet_dependencies = False
+            for dependency in dependencies[job_description.name.lower()]:
+                if dependency in known_jobs and dependency not in result_map:
+                    has_unmet_dependencies = True
+                    break
 
-        (fd, file_path) = tempfile.mkstemp(suffix='.sh', prefix='tmp')
-        script = job_description.script
-        script = script.format(**script_context)
-        os.write(fd, b"#!/bin/bash -e\n")
-        os.write(fd, script.replace('\r', '\n').encode('utf-8'))
-        os.close(fd)
+            if has_unmet_dependencies:
+                jobs_left_to_do.append(job_description)
+                continue
 
-        st = os.stat(file_path)
-        os.chmod(file_path, st.st_mode | stat.S_IEXEC)
+            job = Job(build=build, name=job_description.name)
+            job.status = Job.Status.RUNNING
+            job.log_file.save('build-{:05d}-{:05d}-{}.log'
+                              .format(build.project.id,
+                                      build.build_id,
+                                      job_description.name),
+                              File(StringIO()))
+            job.save()
 
-        env_removals = ['DJANGO_SETTINGS_MODULE', 'VIRTUAL_ENV',
-                        'CD_VIRTUAL_ENV']
-        env = dict(os.environ)
-        for env_entry in env_removals:
-            if env_entry in env:
-                env.pop(env_entry)
+            failed_dependencies = []
+            unknown_dependencies = []
+            with open(job.log_file.path, 'a') as log_file:
+                for dependency in dependencies[job_description.name.lower()]:
+                    if dependency not in known_jobs:
+                        log_file.write(
+                            "dependency '{}' unknown, not running '{}'\n"
+                            .format(dependency, job_description.name))
+                        unknown_dependencies.append(dependency)
+                    elif result_map[dependency].result == Job.Result.FAILURE:
+                        log_file.write(
+                            "dependency '{}' failed, not running '{}'\n"
+                            .format(dependency, job_description.name))
+                        failed_dependencies.append(dependency)
 
-        out = open(job.log_file.path, 'w')
-        try:
-            result = subprocess.check_call(
-                [file_path],
-                stdout=out,
-                stderr=out,
-                env=env)
-        except subprocess.CalledProcessError as e:
-            result = e.returncode
+            if failed_dependencies or unknown_dependencies:
+                job.status = Job.Status.FINISHED
+                job.end_time = timezone.now()
+                job.result = Job.Result.FAILURE
+                job.save()
 
-        out.close()
+                all_passed = False
 
-        job.status = Job.Status.FINISHED
-        job.end_time = timezone.now()
+                if job_description.report_result:
+                    info = []
+                    if failed_dependencies:
+                        info.append('failed: ' + ', '.join(failed_dependencies))
+                    if unknown_dependencies:
+                        info.append('unknown: ' + ', '.join(unknown_dependencies))
+                    details = '; '.join(info)
+                    reportable_job_finished.send('core', job=job, details=details)
 
-        if not result:
-            job.result = Job.Result.SUCCESS
-        else:
-            job.result = Job.Result.FAILURE
-            all_passed = False
+                result_map[job_description.name.lower()] = job
+                continue
 
-        details = job.get_duration()
+            (fd, file_path) = tempfile.mkstemp(suffix='.sh', prefix='tmp')
+            script = job_description.script
+            script = script.format(**script_context)
+            os.write(fd, b"#!/bin/bash -e\n")
+            os.write(fd, script.replace('\r', '\n').encode('utf-8'))
+            os.close(fd)
 
-        job.save()
-        if job_description.report_result:
-            reportable_job_finished.send('core', job=job, details=details)
+            st = os.stat(file_path)
+            os.chmod(file_path, st.st_mode | stat.S_IEXEC)
+
+            env_removals = ['DJANGO_SETTINGS_MODULE', 'VIRTUAL_ENV',
+                            'CD_VIRTUAL_ENV']
+            env = dict(os.environ)
+            for env_entry in env_removals:
+                if env_entry in env:
+                    env.pop(env_entry)
+
+            out = open(job.log_file.path, 'w')
+            try:
+                result = subprocess.check_call(
+                    [file_path],
+                    stdout=out,
+                    stderr=out,
+                    env=env)
+            except subprocess.CalledProcessError as e:
+                result = e.returncode
+
+            out.close()
+
+            job.status = Job.Status.FINISHED
+            job.end_time = timezone.now()
+
+            if not result:
+                job.result = Job.Result.SUCCESS
+            else:
+                job.result = Job.Result.FAILURE
+                all_passed = False
+
+            details = job.get_duration()
+
+            job.save()
+            if job_description.report_result:
+                reportable_job_finished.send('core', job=job, details=details)
+
+            result_map[job_description.name.lower()] = job
+
+        if jobs_to_do == jobs_left_to_do:
+            # this shouldn't happen, but to avoid an endless loop,
+            # let's fail hard here
+            raise Exception('iteration over jobs_to_do made no progress: {}'.format(jobs_to_do))
+
+        jobs_to_do = jobs_left_to_do
 
     if all_passed:
         build.result = Build.Result.SUCCESS
